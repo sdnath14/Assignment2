1a554757ace6de7f94a08821d5d40a5024c5bed5
Sneha Debnath <149677420+sdnath14@users.noreply.github.com>
2025-01-14 19:49:17 +0530

    Update q7.java

--- q7.java
+++ q7.java
@@ -1,28 +1,38 @@
 import java.util.Scanner;
 
 class MobileShop {
-    public void checkitavailable(int versionandroid) {
-        if (versionandroid == 15) {
+    private int versionAndroid;
+
+    public int getVersionAndroid() {
+        return versionAndroid;
+    }
+
+    public void setVersionAndroid(int versionAndroid) {
+        this.versionAndroid = versionAndroid;
+    }
+
+    public void checkAvailability() {
+        if (versionAndroid == 15) {
             System.out.println("I will purchase it.");
         } else {
             System.out.println("No thanks!!");
         }
     }
-
 }
 
 public class q7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String models;
-        int versionandroid;
-        System.out.println("Enter sir which model you want?");
+
+        System.out.println("Enter sir, which model you want?");
+        String models = sc.nextLine();
+
         System.out.println("Which android version you want?");
-        models = sc.nextLine();
-        versionandroid = sc.nextInt();
+        int versionAndroid = sc.nextInt();
+
         MobileShop m = new MobileShop();
-        m.checkitavailable(versionandroid);
+        m.setVersionAndroid(versionAndroid);
 
+        m.checkAvailability();
     }
-
 }
