73bb4bbbd571e030579ec5595899ee693eb8beae
Sneha Debnath <149677420+sdnath14@users.noreply.github.com>
2025-01-14 19:45:21 +0530

    Update q4.java

--- q4.java
+++ q4.java
@@ -1,34 +1,57 @@
 class LED {
-    int id;
-    double price;
-    String model;
+    private int id;
+    private double price;
+    private String model;
 
-    public LED(int id, double price, String model) {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
         this.id = id;
-        this.model = model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
         this.price = price;
     }
 
-    void display() {
-        System.out.println("model:" + model + "id:" + id + "price:" + price);
+    public String getModel() {
+        return model;
     }
 
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    void display() {
+        System.out.println("Model: " + model + ", ID: " + id + ", Price: " + price);
+    }
 }
 
 public class q4 {
     public static void main(String[] args) {
-        LED model1 = new LED(001, 40000, "Sony");
-        LED model2 = new LED(003, 50000, "Samsung");
+        LED model1 = new LED();
+        model1.setId(1);
+        model1.setPrice(40000);
+        model1.setModel("Sony");
 
-        if (model1.price > model2.price) {
-            model1.model = "Sony Premium Tv";
+        LED model2 = new LED();
+        model2.setId(3);
+        model2.setPrice(50000);
+        model2.setModel("Samsung");
+
+        if (model1.getPrice() > model2.getPrice()) {
+            model1.setModel("Sony Premium Tv");
         } else {
-            model2.model = "Samsung Premium Tv";
+            model2.setModel("Samsung Premium Tv");
         }
 
         model1.display();
         model2.display();
-
     }
-
 }
+
